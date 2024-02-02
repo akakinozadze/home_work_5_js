c3822ff65d4ed345c7bdfb796fa41f2882c308da
akakinozadze <145987347+akakinozadze@users.noreply.github.com>
2024-02-02 16:37:16 +0400

    4

--- js/script.js
+++ js/script.js
@@ -8,29 +8,36 @@
 
 //! 2 //
 
-let newElement = document.createElement("img");
-newElement.classList.add("wraper");
-document.getElementById("box1").appendChild(newElement);
-newElement.setAttribute(
-  "src",
-  "https://i0.wp.com/www.trafalgar.com/real-word/wp-content/uploads/sites/3/2020/01/lisbon-portugal-skyline-JHVLYKP.jpg?fit=1214%2C810&ssl=1"
-);
-newElement.setAttribute("alt", "image.oldest.country");
-let newElement2 = document.createElement("h2"); 
-document.getElementById("box1").appendChild(newElement2);
-newElement2.classList.add("title");
-newElement2.innerText="image title ";
-newElement2.style.color = "red";
-newElement2.style.fontSize = "30px";
+// let newElement = document.createElement("img");
+// newElement.classList.add("wraper");
+// document.getElementById("box1").appendChild(newElement);
+// newElement.setAttribute(
+//   "src",
+//   "https://i0.wp.com/www.trafalgar.com/real-word/wp-content/uploads/sites/3/2020/01/lisbon-portugal-skyline-JHVLYKP.jpg?fit=1214%2C810&ssl=1"
+// );
+// newElement.setAttribute("alt", "image.oldest.country");
+// let newElement2 = document.createElement("h2");
+// document.getElementById("box1").appendChild(newElement2);
+// newElement2.classList.add("title");
+// newElement2.textContent="image title ";
+// newElement2.style.color = "red";
+// newElement2.style.fontSize = "30px";
 
-//! 3 //
+// //! 3 //
 
-let newElement3 = document.querySelectorAll(".box2");
+// let newElement3 = document.querySelectorAll(".box2");
 
-newElement3.forEach(function(item){
-    let element1 = document.createElement("p");
-    element1.classList.add("text2");
-    element1.textContent = "hello";
-    
-    item.appendChild(element1);
-})
+// newElement3.forEach(function(item){
+//     let element1 = document.createElement("p");
+//     element1.classList.add("text2");
+//     element1.textContent = "hello";
+
+//     item.appendChild(element1);
+// })
+
+//! 4 //
+
+let array6 = [-1, -2, -3, 4].some(function (numbers) {
+  return numbers > 0;
+});
+console.log(array6);
